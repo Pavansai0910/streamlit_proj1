d2e831ed0170a96e0dade1c8d25a9e19413b2dfb
Pavansai Gude <113626349+Pavansai0910@users.noreply.github.com>
2023-08-27 10:46:16 +0530

    Add files via upload

---
+++ inn_streamlit_1-main/assign1.py
@@ -0,0 +1,5 @@
+import streamlit as st 
+st.title(':red[Hello!]:wave:... I\'m Priyanka Ippala')
+st.subheader('Let me tell u something....I made this entire webpage Just by using Streamlit.')
+st.subheader('Is\'nt it cool to design such a page with just using  a library of pyhton...:smile:')
+st.caption('Visit sripts.io page to learn more')
